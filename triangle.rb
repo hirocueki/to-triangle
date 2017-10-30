eaef245fa645ba12b32f4ce570ff7151762bba38
Hiroshi Ueki <hirocueki@gmail.com>
2017-10-30 17:26:37 +0900

    テストケースの追加とTriangleの実装

--- triangle.rb
+++ triangle.rb
@@ -1,5 +1,20 @@
 class Triangle
     def self.ask(a,b,c)
+        return "三角形じゃないです＞＜" unless is_triangle(a,b,c)
+        return "正三角形ですね！" if is_squere(a,b,c)
+        return "二等辺三角形ですね！" if is_isosceles(a,b,c)
         "不等辺三角形ですね！"
+    end
+    def self.is_isosceles(a,b,c)
+        return false if is_squere(a,b,c)
+        a == b || b==c || a==c
+    end
+    def self.is_squere(a,b,c)
+        a == b && a == c
+    end
+    def self.is_triangle(a,b,c)
+        a < b+c &&
+        b < a+c &&
+        c < a+b
     end
 end
